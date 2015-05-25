6fd9c5a8674db50bea04debcabe24152b025f1e9
Amir M-Aghaei <amir@localhost.localdomain>
2015-05-25 12:38:28 -0700

    Add emacs personal file.

---
+++ emacs.d/personal/personal.el
@@ -0,0 +1,13 @@
+;;; personal.el --- personal stuff to add to emacs prelude
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+;; Enabling CDLatex for org mode-line
+(add-hook 'org-mode-hook 'turn-on-org-cdlatex)
+
+(provide 'personal)
+
+;;; personal.el ends here
