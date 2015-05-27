1b0c87335ef86b80c7c2a5e339b30d1f572cc0fa
Amir M-Aghaei <amir.maghaei@gmail.com>
2015-05-26 18:30:30 -0700

    Add git and emacs plugin to fish.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,12 +2,13 @@
 set fish_path $HOME/.oh-my-fish
 
 # Theme
-set fish_theme robbyrussell
+# set fish_theme robbyrussell
+set fish_theme agnoster
 
 # All built-in plugins can be found at ~/.oh-my-fish/plugins/
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme
+set fish_plugins theme git emacs
 
 # Path to your custom folder (default path is ~/.oh-my-fish/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
