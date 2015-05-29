cdbd402c68ac4439390605e90137e4d4517b28b5
Amir M-Aghaei <amir@localhost.localdomain>
2015-05-28 21:22:55 -0700

    Enabling yasnippet mode in emacs.

--- emacs.d/personal/personal.el
+++ emacs.d/personal/personal.el
@@ -8,6 +8,12 @@
 ;; Enabling CDLatex for org mode-line
 (add-hook 'org-mode-hook 'turn-on-org-cdlatex)
 
+;; Adding packages
+(prelude-require-package 'yasnippet)
+
+;; Enabling yasnippet global mode
+(yas-global-mode +1)
+
 (provide 'personal)
 
 ;;; personal.el ends here
