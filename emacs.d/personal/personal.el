4121db0e34292c1b0d13defd907d2174738d9ce6
Amir M-Aghaei <amir.maghaei@gmail.com>
2018-01-16 16:03:39 -0800

    remove transparency for emacs.

--- emacs.d/personal/personal.el
+++ emacs.d/personal/personal.el
@@ -55,8 +55,8 @@
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 ;; transparency!!
-(set-frame-parameter (selected-frame) 'alpha '(90 . 50))
-(add-to-list 'default-frame-alist '(alpha . (90 . 50)))
+;; (set-frame-parameter (selected-frame) 'alpha '(90 . 50))
+;; (add-to-list 'default-frame-alist '(alpha . (90 . 50)))
 
 (provide 'personal)
 
