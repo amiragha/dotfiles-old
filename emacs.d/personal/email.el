6a7683442355e0945bdc67ab3725cb6a3beaad9b
Amir M-Aghaei <amir.maghaei@gmail.com>
2018-01-21 14:26:36 -0800

    Add email setup to emacs.

---
+++ emacs.d/personal/email.el
@@ -0,0 +1,90 @@
+;; included in emacs >= 26 under the name auth-source-pass
+(prelude-require-package 'auth-password-store)
+(auth-pass-enable)
+
+(require 'mu4e)
+
+;; default
+;; (setq mu4e-maildir "~/Maildir")
+(setq mu4e-maildir "~/Gmail")
+
+(setq mu4e-drafts-folder "/[Gmail].Drafts")
+(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+(setq mu4e-trash-folder  "/[Gmail].Trash")
+
+;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+(setq mu4e-sent-messages-behavior 'delete)
+
+;; (See the documentation for `mu4e-sent-messages-behavior' if you have
+;; additional non-Gmail addresses and want assign them different
+;; behavior.)
+
+;; setup some handy shortcuts
+;; you can quickly switch to your Inbox -- press ``ji''
+;; then, when you want archive some messages, move them to
+;; the 'All Mail' folder by pressing ``ma''.
+
+(setq mu4e-maildir-shortcuts
+      '( ("/inbox"               . ?i)
+         ("/[Gmail].Sent Mail"   . ?s)
+         ("/[Gmail].Trash"       . ?t)
+         ("/[Gmail].All Mail"    . ?a)))
+
+;; allow for updating mail using 'U' in the main view:
+(setq mu4e-get-mail-command "offlineimap -o"
+      mu4e-update-interval 300)
+
+;; customize the reply-quote-string
+;; M-x find-function RET message-citation-line-format for docs
+(setq message-citation-line-format "[%Y-%m-%d %H:%M %Z] %N writes:\n")
+(setq message-citation-line-function 'message-insert-formatted-citation-line)
+;; (setq message-indentation-spaces 2)
+
+;; something about ourselves
+(setq user-mail-address "amir.maghaei@gmail.com"
+      user-full-name  "Amir M-Aghaei"
+      mu4e-compose-signature
+      (concat
+       "Amir\n"
+       ""))
+
+;; sending mail -- replace USERNAME with your gmail username
+;; also, make sure the gnutls command line utils are installed
+;; package 'gnutls-bin' in Debian/Ubuntu
+
+(require 'smtpmail)
+;; (setq message-send-mail-function 'smtpmail-send-it
+;;       starttls-use-gnutls t
+;;       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+;;       smtpmail-auth-credentials
+;;       '(("smtp.gmail.com" 587 "amir.maghaei@gmail.com" nil))
+;;       smtpmail-default-smtp-server "smtp.gmail.com"
+;;       smtpmail-smtp-server "smtp.gmail.com"
+;;      smtpmail-smtp-service 587)
+
+;; alternatively, for emacs-24 you can use:
+(setq message-send-mail-function 'smtpmail-send-it
+    smtpmail-stream-type 'starttls
+    smtpmail-default-smtp-server "smtp.gmail.com"
+    smtpmail-smtp-server "smtp.gmail.com"
+    smtpmail-smtp-service 587)
+
+;; don't keep message buffers around
+(setq message-kill-buffer-on-exit t)
+
+(require 'gnus-dired)
+;; make the `gnus-dired-mail-buffers' function also work on
+;; message-mode derived modes, such as mu4e-compose-mode
+(defun gnus-dired-mail-buffers ()
+  "Return a list of active message buffers."
+  (let (buffers)
+    (save-current-buffer
+      (dolist (buffer (buffer-list t))
+        (set-buffer buffer)
+        (when (and (derived-mode-p 'message-mode)
+                   (null message-sent-message-via))
+          (push (buffer-name buffer) buffers))))
+    (nreverse buffers)))
+
+(setq gnus-dired-mail-mode 'mu4e-user-agent)
+(add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
